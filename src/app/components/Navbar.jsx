f14cda99915cfae02d283b33478877d030fabbe9
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-07-31 23:21:05 +0100

    Add React Bootrap, add navBar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,26 +5,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function BasicExample() {
+  const noBorders = { border: 'none' };
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar collapseOnSelect expand="sm" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">ANDY DART</Navbar.Brand>
+        <Navbar.Toggle style={noBorders} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#art">Art</Nav.Link>
+            <Nav.Link href="#exhibitions">Exhibitions</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
