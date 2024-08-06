0aeeee2a299ded6758912666d915feb93a544641
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-08-06 20:22:42 +0100

    add bg main img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import pic_1_background from '../../public/pic_1_background.jpg';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>ANDY DART</p>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Image
+        src={pic_1_background}
+        alt="background"
+        style={{ height: '100vh', width: '100wh' }}
+        className="object-cover sm:object-fill"
+      ></Image>
     </main>
   );
 }
