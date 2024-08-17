f17b31feb51a791ef9dc8d7ba9fe48f1b442cba4
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-08-17 19:48:20 +0100

    Add basic form name/email/message

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,20 +1,56 @@
 'use client';
-
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-function FormForMessage() {
+function FormMessage() {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      {/* name */}
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom01">
+          <Form.Label>Your name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Enter your full name"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      {/* email */}
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your emil address</Form.Label>
+        <Form.Control required type="email" placeholder="Enter your email" />
       </Form.Group>
+      {/* message */}
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Label>Your message</Form.Label>
+        <Form.Control
+          required
+          type="text"
+          placeholder="Enter your full name"
+          as="textarea"
+          rows={3}
+        />
       </Form.Group>
+      <Button type="submit">Submit message</Button>
     </Form>
   );
 }
 
-export default FormForMessage;
+export default FormMessage;
