34b36dd22801dd19a300916161da2a2841b3d13f
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-09-22 19:06:40 +0100

    Add basic grid. img size need fixing

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -4,9 +4,14 @@ import Image from 'next/image';
 
 const Art = () => {
   return (
-    <div className="">
+    <div className="grid justify-items-center grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6  gap-1">
       {imglist.map((img) => (
-        <Image alt={img.alt} src={img.src} key={img.id}></Image>
+        <Image
+          className="w-96 h-96 object-cover"
+          alt={img.alt}
+          src={img.src}
+          key={img.id}
+        ></Image>
       ))}
     </div>
   );
