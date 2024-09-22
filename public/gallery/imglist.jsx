dbc66dab8df70f372085f752a7fea8cf67f28e5a
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-09-22 18:16:33 +0100

    create gallery

---
+++ public/gallery/imglist.jsx
@@ -0,0 +1,52 @@
+import image1 from './pic1_sickbags at the ready_7 1.jpg';
+import image2 from './pic2_green_into_pink_on_red_2020 2.jpg';
+import image3 from './pic3_abstract_painting_2021 1.jpg';
+import image4 from './pic4_blue_yellow_and_rust_on_orange_and_green 1.jpg';
+import image5 from './pic5_jewel_5 2.jpg';
+import image6 from './pic6_untitled.jpg';
+import image7 from './pic7_forgint_the_u.jpg';
+import image8 from './pic8_blue_aqua.jpg';
+
+const paintingsImgs = [
+  {
+    id: '1',
+    src: image1,
+    alt: 'pic1',
+  },
+  {
+    id: '2',
+    src: image2,
+    alt: 'pic2',
+  },
+  {
+    id: '3',
+    src: image3,
+    alt: 'pic3',
+  },
+  {
+    id: '4',
+    src: image4,
+    alt: 'pic4',
+  },
+  {
+    id: '5',
+    src: image5,
+    alt: 'pic5',
+  },
+  {
+    id: '6',
+    src: image6,
+    alt: 'pic6',
+  },
+  {
+    id: '7',
+    src: image7,
+    alt: 'pic7',
+  },
+  {
+    id: '8',
+    src: image8,
+    alt: 'pic8',
+  },
+];
+export default paintingsImgs;
--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import imglist from '../../../public/gallery/imglist';
+import Image from 'next/image';
 
 const Art = () => {
-  return <div>Art</div>;
+  return (
+    <div className="">
+      {imglist.map((img) => (
+        <Image alt={img.alt} src={img.src} key={img.id}></Image>
+      ))}
+    </div>
+  );
 };
 
 export default Art;
