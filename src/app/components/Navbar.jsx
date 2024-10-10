9b9d992707b9839edb0e91460576a158663e789d
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2024-10-10 14:00:13 +0100

    Add Link component to avoid autoloading page when link redirect to itself

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -2,8 +2,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import NavbarToggle from 'react-bootstrap/NavbarToggle';
+import Link from 'next/link';
 
 function NavigationBar() {
   const noBorders = { border: 'none' };
@@ -11,16 +10,25 @@ function NavigationBar() {
     <Navbar collapseOnSelect expand="sm">
       {/* centerOverride overrides Bootstrap */}
       <Container className="flex align-middle centerOverride py-2 my-2">
-        <Navbar.Brand href="/" className="sm pl-1 ">
+        <Navbar.Brand href="/" className="pl-1 ">
           ANDY DART
         </Navbar.Brand>
         <Navbar.Toggle style={noBorders} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="grow-0" id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="art">Art</Nav.Link>
-            <Nav.Link href="#exhibitions">Exhibitions</Nav.Link>
-            <Nav.Link href="contact">Contact</Nav.Link>
+            {/* using LINK for client navigation / avoid loading again when link redirect to itself */}
+            <Link href="/about" passHref legacyBehavior>
+              <Nav.Link>About</Nav.Link>
+            </Link>
+            <Link href="/art" passHref legacyBehavior>
+              <Nav.Link>Art</Nav.Link>
+            </Link>
+            <Link href="#exhibitions" passHref legacyBehavior>
+              <Nav.Link>Exhibitions</Nav.Link>
+            </Link>
+            <Link href="/contact" passHref legacyBehavior>
+              <Nav.Link>Contact</Nav.Link>
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
