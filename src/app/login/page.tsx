f656880c819a262ed0fcd335cbd67d299d68c0d0
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2025-01-23 16:02:59 +0100

    Pass formData to login function when login form is submitted

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,53 @@
+'use client';
+import { useRef, useState } from 'react';
 import { login, signup } from './actions';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function LoginPage() {
+  const [loading, setLoading] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const formData = new FormData(formRef.current!);
+
+    try {
+      await login(formData);
+    } catch (error) {
+      console.error('Login failed:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <form className="mb-3">
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      {/* <button formAction={signup}>Sign up</button> */}
-    </form>
+    <main className="grid justify-items-center">
+      <Form ref={formRef} noValidate onSubmit={handleLogin}>
+        <Form.Group className="mb-3" controlId="validationName">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="validationPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+          />
+        </Form.Group>
+        <Button type="submit" disabled={loading}>
+          {loading ? 'Logging in...' : 'Log in'}
+        </Button>
+      </Form>
+    </main>
   );
 }
