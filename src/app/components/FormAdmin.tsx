d2a6b1c68890ce8a4dbd485049cd0dc688a6f157
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2025-01-27 11:16:25 +0100

    Add preview img inside form

--- src/app/components/FormAdmin.tsx
+++ src/app/components/FormAdmin.tsx
@@ -2,24 +2,68 @@
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
+import Image from 'next/image';
 
 function FormAdmin() {
-  const form = useRef();
+  const form = useRef<HTMLFormElement>(null);
+  const [imgFile, setImgFile] = useState<string>('');
+
+  const addPainting = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(form.current!);
+    const file = {
+      name: data.get('name') as string,
+      info: data.get('info') as string,
+      img: data.get('file'),
+    };
+
+    // console.log(imgFile);
+    // console.log(file.img);
+  };
+
+  const updateImgPreview = (event: React.ChangeEvent<HTMLFormElement>) => {
+    // console.log(imgFile);
+    // event.preventDefault();
+    const file = event.target.files[0];
+    console.log(file);
+    const fileUrl = URL.createObjectURL(file);
+    setImgFile(fileUrl);
+    console.log(fileUrl);
+  };
 
   return (
-    <Form ref={form} className="mt-8">
+    <Form
+      ref={form}
+      className="mt-8"
+      onSubmit={addPainting}
+      onChange={updateImgPreview}
+    >
       <Form.Group className="mb-3" controlId="formPaintingName">
         <Form.Label>Painting name</Form.Label>
-        <Form.Control type="text" placeholder="Enter painting name" />
+        <Form.Control
+          type="text"
+          name="name"
+          placeholder="Enter painting name"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formPaintingInfo">
         <Form.Label>Info</Form.Label>
-        <Form.Control type="text" placeholder="Info" />
+        <Form.Control type="text" name="info" placeholder="Info" />
       </Form.Group>
+      {imgFile && (
+        <div>
+          <Image
+            alt="image preview"
+            width={200}
+            height={200}
+            src={imgFile}
+          ></Image>
+        </div>
+      )}
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Choose file</Form.Label>
-        <Form.Control type="file" />
+        <Form.Control name="file" type="file" />
       </Form.Group>
       <Button className="bg-green-700" variant="outline-primary" type="submit">
         Upload
