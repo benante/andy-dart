89959cafcfdfa4119b65cec7577bd42d46b557b9
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2025-02-05 23:31:10 +0100

    Fix alert pop ups, need refactoring

--- src/app/components/DeleteAdmin.tsx
+++ src/app/components/DeleteAdmin.tsx
@@ -1,7 +1,8 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import Image from 'next/image';
 import { deleteBucketRow, deleteInfo } from '@/utils/supabase/db';
 
@@ -18,12 +19,22 @@ type DivProps = {
   art_work: Painting[];
 };
 
-const deletePainting = (imgFileName: string, id: string) => {
-  deleteBucketRow(imgFileName);
-  deleteInfo(id);
-};
-
 const DeleteDivAdmin: React.FC<DivProps> = ({ art_work }) => {
+  {
+    /* Giving the showAlert variable only a boolean value would make all the alerts triggered/displayed as soon as a single element gets clicked.
+    Instead of creating a component (to be added inside the ListGroup.Item)
+    that would handle the state, here I create an object of arrays, which first element is the id and gets a boolean assigned.
+    Later in the JSX code if the element(painting)'s id is true the alert gets displayed, otherwise it doesn' t.
+    If an alert is open but another item element btn gets clicked, a new object array is created, causing the first alert to disappear  */
+  }
+  const [showAlert, setShowAlert] = useState<{ [id: string]: boolean }>({});
+
+  const deletePainting = (imgFileName: string, id: string) => {
+    deleteBucketRow(imgFileName);
+    deleteInfo(id);
+    setShowAlert({ [id]: false });
+  };
+
   return (
     <ListGroup>
       {art_work.map((painting) => (
@@ -31,20 +42,43 @@ const DeleteDivAdmin: React.FC<DivProps> = ({ art_work }) => {
           key={painting.id}
           className="flex-override items-center justify-between "
         >
-          <Image
-            src={painting.url}
-            width={50}
-            height={50}
-            alt={painting.alt}
-            priority={true}
-          ></Image>
-          <span className="truncate-text">{painting.name}</span>
-          <Button
-            variant="danger"
-            onClick={() => deletePainting(painting.imgFileName, painting.id)}
-          >
-            Remove
-          </Button>
+          {showAlert[painting.id] ? (
+            <Alert className="grow " show={true} variant="warning">
+              <p>Are you sure you want to remove this item?</p>
+              <Button
+                className="mr-2"
+                variant="outline-danger"
+                onClick={() =>
+                  deletePainting(painting.imgFileName, painting.id)
+                }
+              >
+                Yes
+              </Button>
+              <Button
+                variant="outline-primary"
+                onClick={() => setShowAlert({ [painting.id]: false })}
+              >
+                No
+              </Button>
+            </Alert>
+          ) : (
+            <>
+              <Image
+                src={painting.url}
+                width={50}
+                height={50}
+                alt={painting.alt}
+                priority={true}
+              ></Image>
+              <span className="truncate-text">{painting.name}</span>
+              <Button
+                variant="outline-danger"
+                onClick={() => setShowAlert({ [painting.id]: true })}
+              >
+                Remove
+              </Button>
+            </>
+          )}
         </ListGroup.Item>
       ))}
     </ListGroup>
